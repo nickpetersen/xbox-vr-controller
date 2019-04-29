6b489697ed2d1839178bcc38bec3732398287d87
Nick Petersen <nick@nickpetersen.co>
2019-04-29 12:24:00 -0500

    added x, y limits and locked z axis at 0

--- xboxRotate.cs
+++ xboxRotate.cs
@@ -9,14 +9,26 @@ public class xboxRotate : MonoBehaviour
     public float minFov = 15f;
     public float maxFov = 90f;
     public float sensitivity = 10f;
+    public float rotationX = 0f;
+    public float rotationY = 90f;
+    public float sensitivityX = 2f;
+    public float sensitivityY = 2f;
+    public float lockPos = 0;
 
     void Update()
     {
+
         // Rotate Left to Right
-        float rotation = Input.GetAxis("LeftJoystickHorizontal") * rotationSpeed;
-        rotation *= Time.deltaTime;
-        transform.Rotate(0, rotation, 0);
+        rotationY += Input.GetAxis("LeftJoystickHorizontal") * sensitivityY;
+
+        // Rotate Up and Down
+        rotationX += Input.GetAxis("LeftJoystickVertical") * sensitivityX;
+        rotationX = Mathf.Clamp (rotationX, -50, 40);
         
+        // Vertical Boundaries
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, lockPos);
+        transform.localEulerAngles = new Vector3(-rotationX, rotationY, 0f);
+
         // Zoom In and Out
         float fov = Camera.main.fieldOfView;
         fov += Input.GetAxis("RightJoystickVertical") * sensitivity;
